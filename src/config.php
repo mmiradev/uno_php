99b1f4c57c87aeb55feaf527504dedfde15bba9a
mmiradev <mmirgal868@g.educaand.es>
2023-12-07 19:21:24 +0100

    fixed config

--- src/config.php
+++ src/config.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO('mysql:host=127.0.0.1;port=33060;dbname=oremessenger',
-    'root', 'ejemplo_pass');
+$db = new PDO('mysql:host=127.0.0.1;port=33062;dbname=uno',
+    'root', 'uno_mmg');
 
 session_start();
