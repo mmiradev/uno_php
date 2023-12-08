14ab760b43cb012afd7e4ffd11c366d9bb0e8f92
mmiradev <mmirgal868@g.educaand.es>
2023-12-08 18:28:12 +0100

    filled Card object class

--- src/Pile.php
+++ src/Pile.php
@@ -2,5 +2,26 @@
 
 class Pile
 {
+    //atributes
+    protected Card $pile;
 
+    //Constructor
+    public function __construct(Card $pile){
+        $this->pile = [];
+    }
+
+    //getters
+    public function getPile(): Card{
+        return $this->pile;
+    }
+
+    public function addCard(Card $card): void{
+        if (empty($card)) throw new Exception("Invalid card - cannot be empty!");
+        array_push($this->pile, $card);
+    }
+
+    public function shuffle() : void
+    {
+        shuffle($this->pile);
+    }
 }
