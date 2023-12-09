5bb33a82d88df3a39715b301fef44a76c797be74
mmiradev <mmirgal868@g.educaand.es>
2023-12-09 14:28:14 +0100

    implemented functionality on DrawPile.php

--- src/DrawPile.php
+++ src/DrawPile.php
@@ -2,5 +2,66 @@
 
 class DrawPile extends Pile
 {
+    protected Card $pile;
+
+    public function __construct(Card $pile)
+    {
+        parent::__construct($pile);
+        $this->initializeDrawPile();
+    }
+
+    private function initializeDrawPile() {
+        $colors = [Card::RED, Card::BLUE, Card::GREEN, Card::YELLOW];
+        $specialSymbols = [Card::SKIP, Card::REVERSE, Card::PICKER];
+        $symbols = range(0, 9);
+
+        //normal cards (only one 0 for each color a doubles for the rest)
+        foreach ($colors as $color) {
+            foreach ($symbols as $symbol) {
+                if ($symbol == 0) {
+                    $this->addCard(new Card($color, $symbol));
+                } else {
+
+                    $this->addCard(new Card($color, $symbol));
+                    $this->addCard(new Card($color, $symbol));
+                }
+            }
+        }
+
+        //now adding special cards
+        foreach ($colors as $color) {
+            foreach ($specialSymbols as $symbol) {
+                $this->addCard(new Card($color, $symbol));
+                $this->addCard(new Card($color, $symbol));
+            }
+        }
+
+        for ($i = 0; $i < 4; $i++) {
+                $this->addCard(new Card(Card::BLACK, Card::COLOR_CHANGER));
+            $this->addCard(new Card(Card::BLACK, Card::PICK_FOUR));
+        }
+
+        $this->shuffle();
+    }
+
+
+    //methods and actions
+    public function drawCard() {
+        if (empty($this->cards)) {
+            throw new Exception("El mazo está vacío");
+        }
+
+        return array_shift($this->cards);
+    }
+
+    public function addPileButLast(Pile $p) {
+        if (count($p->getPile()) > 1) {
+            $this->cards = array_merge($this->cards, array_slice((array)$p->getPile(), 0, -1));
+        }
+    }
+
+    public function countCards() {
+        return $this->pile->count();
+    }
 
 }
