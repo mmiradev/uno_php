c165636cfe04413b31085f8cce812f12d326eaff
mmiradev <mmirgal868@g.educaand.es>
2023-12-09 19:57:15 +0100

    implement functionability to PlayerPile.php

--- src/PlayerPile.php
+++ src/PlayerPile.php
@@ -2,5 +2,24 @@
 
 class PlayerPile extends Pile
 {
+    public function __construct(Card $pile)
+    {
+        parent::__construct($pile);
+    }
 
+    //methods and actions
+    public function getCards() {
+        return $this->pile;
+    }
+
+    public function playCard(Card $c) {
+        $key = array_search($c, $this->pile, true);
+
+        if ($key !== false) {
+            // La carta está en el mazo, la eliminamos
+            array_splice($this->pile, $key, 1);
+        } else {
+            throw new Exception("La carta no está en el mazo");
+        }
+    }
 }
