cb603c3dd90a11a3ff55d9fbae70a4cb0f2b5103
mmiradev <mmirgal868@g.educaand.es>
2023-12-09 19:57:33 +0100

    implement functionability to DiscardPile.php.php

--- src/DiscardPile.php
+++ src/DiscardPile.php
@@ -2,5 +2,24 @@
 
 class DiscardPile extends Pile
 {
+    public function __construct(Card $pile)
+    {
+        parent::__construct($pile);
+    }
 
+    //methods and actions
+    public function peekLastCard() {
+        $lastIndex = count($this->pile) - 1;
+        if ($lastIndex >= 0) {
+            return $this->pile[$lastIndex];
+        } else {
+            throw new Exception("The pile is empty");
+        }
+    }
+
+    public function clear() {
+        if (count($this->pile) > 1) {
+            $this->pile = [$this->pile[count($this->pile) - 1]];
+        }
+    }
 }
